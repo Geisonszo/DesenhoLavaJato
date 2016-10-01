4c08ed6420bc52e7bdb5dbd7493bdcb4fe057df1
geisonszo <geisonszo@gmail.com>
2016-09-30 21:12:07 -0300

    Implemetando classe LavagemViewHolder

--- app/src/main/java/fga/unb/desenho20162/lavajato/Adapter/LavagemRecyclerAdapter.java
+++ app/src/main/java/fga/unb/desenho20162/lavajato/Adapter/LavagemRecyclerAdapter.java
@@ -1,8 +1,4 @@
 package fga.unb.desenho20162.lavajato.Adapter;
 
-/**
- * Created by goliveira on 9/30/16.
- */
-
 public class LavagemRecyclerAdapter {
 }
--- app/src/main/java/fga/unb/desenho20162/lavajato/view/LavagemViewHolder.java
+++ app/src/main/java/fga/unb/desenho20162/lavajato/view/LavagemViewHolder.java
@@ -1,5 +1,31 @@
 package fga.unb.desenho20162.lavajato.view;
 
 
-public class LavagemViewHolder {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import fga.unb.desenho20162.lavajato.R;
+
+public class LavagemViewHolder extends RecyclerView.ViewHolder{
+
+    TextView nome;
+    TextView telefone;
+    TextView tamanho;
+    TextView placa;
+    TextView cor;
+    TextView marca;
+    TextView modelo;
+
+    public LavagemViewHolder ( View itemView ) {
+        super(itemView);
+
+        nome = (TextView) itemView.findViewById(R.id.idNome);
+        telefone = (TextView) itemView.findViewById(R.id.idTelefone);
+        tamanho = (TextView) itemView.findViewById(R.id.idTamanho);
+        placa = (TextView) itemView.findViewById(R.id.idPlaca);
+        cor = (TextView) itemView.findViewById(R.id.idCor);
+        marca = (TextView) itemView.findViewById(R.id.idMarca);
+        modelo = (TextView) itemView.findViewById(R.id.idModelo);
+    }
 }
